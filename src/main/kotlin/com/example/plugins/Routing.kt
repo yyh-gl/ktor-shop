3003aadda191599ccbed9148ba2f8cbb54b47e7f
yyh-gl <yhonda.95.gl@gmail.com>
2022-11-17 01:11:31 +0900

    FreeMarkerを使って商品一覧ページを仮作成

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -2,12 +2,19 @@ package com.example.plugins
 
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
 import io.ktor.server.response.*
+
+data class Content(val id: Int, val name: String, val price: Int)
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("ktor shop")
+            val contents: List<Content> = listOf(
+                Content(1, "赤ペン", 100),
+                Content(2, "青ペン", 200),
+            )
+            call.respond(FreeMarkerContent("contents.ftl", mapOf("contents" to contents)))
         }
     }
 }
---
+++ src/main/resources/templates/contents.ftl
@@ -0,0 +1,11 @@
+<html>
+  <body>
+  <#list contents as content>
+    <div>
+      ${content.id}
+      ${content.name}
+      ${content.price}
+    </div>
+  </#list>
+  </body>
+</html>
